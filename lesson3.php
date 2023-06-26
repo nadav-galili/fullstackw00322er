9888b0071e6e2a58c968c936afbfa917ef0adf4e
Nadav Galili <67638018+nadav-galili@users.noreply.github.com>
2023-06-26 19:12:43 +0300

    initial commit

---
+++ lesson3.php
@@ -0,0 +1,118 @@
+<?php
+
+// $x = 3;
+// $x = $x*2;
+// echo $x.'<br>';
+// $x *=2;
+// echo $x.'<br>';
+// $x /=2;
+// echo $x.'<br>';
+// $x -=2;
+// echo $x;
+// // /////////////////////////////
+
+// $str = 'hello';
+// $str .= 'world';
+// echo $str;
+// ///
+
+// $y +=3;
+// echo $y;
+// echo '<hr>';
+// //power of
+// $res = 2**3;
+// echo $res;
+
+/// spaceship operator  >php 7.0
+/*
+echo 9<=>3 // output 1
+echo 'a' <=> 'A' // output 1
+*/
+
+///ternary operator
+
+// $x = $y? $y :6;
+
+// echo $x;
+
+//null coalesing PHP>=7.00
+
+
+// $title= 'my page';
+
+
+
+// $x = 6;
+
+// unset($x);
+
+// echo $x;
+
+
+//PHP MATH FUNCTIONS
+
+// echo abs(-89.6);// absolute value
+// echo '<hr>';
+// $price = 3.216;
+
+// echo ceil($price);//ceiling-round up
+// //
+// echo '<hr>';
+// echo floor($price);
+// echo '<hr>';
+
+// echo rand(10,22);
+// echo '<hr>';
+
+// echo pi();
+// echo '<hr>';
+
+// echo 2**3;
+// echo '<hr>';
+// echo pow(2, 3);
+
+// echo '<hr>';
+// echo sqrt(81);
+
+//  $price = 3.716;
+
+// echo  round($price, 1);
+
+//STRING FUNCTIONS 
+
+// $user_name = '     avi Cohen     ';
+// $user_name = trim($user_name);
+// var_dump($user_name);
+
+// $str = '!helollo';
+// echo trim($str, '!');
+
+// $str = '   !hell!o!  ';
+// $str = trim($str);
+// $str = trim($str, '!');
+// var_dump($str);
+
+// $str = '   hello    ';
+// $str = rtrim($str);
+// var_dump($str);
+
+
+// $str = '!helllolo';
+// echo strlen($str);
+// echo '<hr>';
+// $mystr = 'שלום';
+// echo mb_strlen($mystr);
+
+// $user = 'Avi cohen';
+// echo $user . '<br>';
+// echo strtoupper($user);
+// echo '<br>';
+// $user_name = 'MOSHE LEVY';
+// echo strtolower($user_name);
+
+// $user_name = 'avi cohen';
+// echo ucwords($user_name);
+
+// $user = 'aVi cOhEn';
+// $user = strtolower($user);
+// echo ucwords($user);
